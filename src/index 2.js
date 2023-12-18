0117bb33123b71c0481f80fcf865e3835c5680be
Sam Ashenafi <sashen1@lsu.edu>
2023-12-18 15:00:37 -0500

    Deployment Version 1

---
+++ src/index 2.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  // You can customize your theme here
+});
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>,
+  document.getElementById('root')
+);
